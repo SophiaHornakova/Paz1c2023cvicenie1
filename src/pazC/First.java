1be372e0b8398291148520e30e002843d9bd64bd
sophi <sophi@SophiHornakNote>
2023-09-19 13:53:37 +0200

    zlata

--- src/pazC/First.java
+++ src/pazC/First.java
@@ -3,6 +3,6 @@ package pazC;
 public class First {
 	public static void main(String[] args) {
 		System.out.println("ahoj");
-		System.out.println("ahoj moja");
+		System.out.println("ahoj moja zlata");
 	}
 }
