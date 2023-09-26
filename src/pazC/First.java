fa17f998754c9efe1aef43153364d85705d8d656
zuzka <zuzka@Zuzka-Acer.mshome.net>
2023-09-26 12:59:18 +0200

    7hyh

--- src/pazC/First.java
+++ src/pazC/First.java
@@ -4,5 +4,6 @@ public class First {
 	public static void main(String[] args) {
 		System.out.println("ahoj");
 		System.out.println("ahoj");
+		System.out.println("Dnes mam VELMI  skvely den");
 	}
 }
